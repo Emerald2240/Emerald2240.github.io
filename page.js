4d0d171679ec9ead486b6b4752fc597e91d494bd
Orji Michael <Orji Michael>
2020-09-30 23:45:09 -0700

    Single portfolio page to be hosted with gitpages

---
+++ page.js
@@ -0,0 +1,20 @@
+// let mobile_bar = document.getElementById("mobile_bar");
+// let nav = document.getElementById("navigation");
+
+// mobile_bar.onclick = function(){
+//      if(nav.style.display == "block"){
+//       nav.style.display  = "none";  
+//      }else{
+//       nav.style.display  = "block";  
+//      }
+
+// }
+
+$(document).ready(function(){
+
+ // nav bar toggle
+ $("#mobile_bar").click(function(){
+     $("#navigation").slideToggle();
+})
+
+})
